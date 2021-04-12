437ca408255fe45db843ccf89695d9070d138fea
Aditya Inapurapu <adityaii@gmail.com>
2021-04-11 20:45:01 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import ssl
+import urllib.request
+import sys
+
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+contents = urllib.request.urlopen("https://chromedriver.storage.googleapis.com/LATEST_RELEASE", context=ctx).read()
+contents = contents.decode("utf-8")
+
+f = open("/Users/adityai/chromedriver.txt", "r")
+last_version = f.read()
+print("---" + last_version + "---")
+print("---" + contents + "---")
+f.close()
+if last_version != contents:
+    f1 = open("/Users/adityai/chromedriver.txt", "w+")
+    f1.write(contents)
+    f1.close()
+    sys.exit(-1)
