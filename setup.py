64fd9b42b51ab20e42e8f1f33a6271adf562f41b
Aditya Inapurapu <aialerts@yahoo.com>
2022-04-14 14:48:23 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='chrome-driver-monitor-basic',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='Aditya Inapurapu',
+    author_email='',
+    description=''
+)
